558ddee9ca26d0e0c37716e4503dc53fbea7ebb6
Jose Nortes <nooors@gmail.com>
2022-12-04 13:50:04 +0100

    add a webclient to consume api resources

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -6,10 +6,17 @@ package com.paymentchain.customer.controller;
 
 import com.paymentchain.customer.entities.Customer;
 import com.paymentchain.customer.repository.CustomerRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.time.Duration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +25,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  *
@@ -30,7 +39,29 @@ public class CustomerRestController {
     @Autowired
     CustomerRepository customerRepository;
     
+    private final WebClient.Builder webClientBuilder;
     
+    public CustomerRestController(WebClient.Builder webClientBuilder){
+        this.webClientBuilder = webClientBuilder;
+    }
+    
+    // webClient requieres HttpClient library to work properly
+    HttpClient client = HttpClient.create()
+            // Connetion Timeout: is a period within whitch a connection between a client an a server must be stablished
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            // Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and write Timeout: A read timeaout occurs when no data was read within a certain
+            // period of time, shile the wirte timeout occurs when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
+            
+            
     @GetMapping()
     public List<Customer> findAll() {
         return customerRepository.findAll();
